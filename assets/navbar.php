1a1cd3653d43a180f2313676dc1f2622eb689e8b
Sibald Gemeen <sibald@sibaldgemeen.com>
2020-10-14 00:44:33 +0200

    Update

---
+++ assets/navbar.php
@@ -0,0 +1,19 @@
+<div class="navbar">
+    <div class="left-side">
+        <img id="logo" src="/assets/images/Ulocation-svg.svg" alt="Ulocation logo">
+        <div class="nav-link-wrapper">
+            <a href="#">Home</a>
+        </div>
+        <div class="nav-link-wrapper">
+            <a href="#">Upload</a>
+        </div>
+        <div class="nav-link-wrapper">
+            <a href="#">About</a>
+        </div>
+    </div>
+    <div class="right-side">
+        <div class="nav-link-wrapper">
+            <a id="contact" href="#">Contact</a>
+        </div>
+    </div>
+</div>
